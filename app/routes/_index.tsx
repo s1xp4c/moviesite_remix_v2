eab7566f444f8a2572810f703b6a653704c15887
s1xp4c <blockstarterinfo@gmail.com>
2023-08-19 01:15:34 +0200

    MovieDB added to index

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,49 @@
-import type { V2_MetaFunction } from "@remix-run/node";
+import { json, type LoaderArgs, type V2_MetaFunction } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
 
 export const meta: V2_MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "RemixV2 Moviesite" },
+    { name: "description", content: "RemixV2 test site!" },
   ];
 };
 
+export async function loader({}: LoaderArgs) {
+  const url = await fetch(
+    'https://api.themoviedb.org/3/trending/movie/day?language=en-US',
+    {
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNzIzMWJiNmE2NzkyOGVkOTIxZTAwN2RiNWYyZGU0MSIsInN1YiI6IjY0ZGZlN2M5MDc2Y2U4MDBjNjQxYzRmMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zv5lnK-KncQOb9mn5Idh_DffMZYnrG2R-wW7_c4SAos',
+      },
+    
+    }
+  );
+  return json(await url.json());
+}
+
 export default function Index() {
+  const data = useLoaderData();
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer noopener"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer noopener"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer noopener">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+<div className="bg-white py-6 sm:py-8 lg:py-12">
+<div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+  <div className="mb-10 md:mb-16">
+    <p className="mb-4 text-center text-3xl font-bold text-gray-800 md:mb-6 lg:text-4xl">
+      Top Trending Movies
+    </p>
+  </div>
+  <div className="grid gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-8">
+{data.results.map((movie: any) => (
+<div key={movie.id} className="flex flex-col overflow-hidden rounded-lg border bg-white">
+  <Link prefetch="intent" className="group relative block h-48 overflow-hidden bg-gray-100 md:h-64" to={`movie/${movie.id}/comments`} >
+<img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={`${movie.id}`} />
+  </Link>
+</div>
+))}
+  </div>
+</div>
+</div>
+    
   );
 }
